d0a19622ae7956b5d41937a6679b471912a36dbb
playfluent <joe@viasana.co>
2024-11-30 17:25:43 -0800

    style updates

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -1,37 +1,59 @@
 'use client';
 
-export default function Hero() {
+export default function Callout() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6">
+    <div className="flex flex-col items-center justify-center p-6">
+      {/* Text Block */}
       <div className="max-w-5xl pl-48">
         <h1 className="text-brand-red font-marker font-bold text-2xl md:text-3xl uppercase mb-3">
           Sustainability isn't about sacrifice.
         </h1>
         <div className="flex flex-col gap-3 font-franklin text-black">
-        <p className=" font-bold text-lg md:text-2xl leading-relaxed">
-          It's about doing more with what you already have.
-        </p>
-        <p>The Spodek Method helps you resolve inner conflicts and tap into your deepest motivations, making sustainable living an empowering and joyful experience. </p>
-        <p className=" font-bold text-lg md:text-2xl leading-relaxed">
-          Begin Your Journey:
-        </p>
-      <div className="flex flex-row justify-between mt-8">
-        <div className="col-1/3">
-          <a href="#" className="text-black font-bold text-xl md:text-xl leading-relaxed">
-            Learn More
-          </a>
-        </div>
-        <div className="col-1/3">
-          <a href="#" className=" font-bold text-xl md:text-xl leading-relaxed">
-            Get Started
-          </a>
-        </div>
-        <div className="col-1/3">
-          <a href="#" className="font-bold text-xl md:text-xl leading-relaxed">
-            Explore Resources
-            </a>
+          <p className=" font-bold text-lg md:text-2xl leading-relaxed">
+            It's about doing more with what you already have.
+          </p>
+          <p>The Spodek Method helps you resolve inner conflicts and tap into your deepest motivations, making sustainable living an empowering and joyful experience. </p>
+          <p className=" font-bold text-lg md:text-2xl">
+            Begin Your Journey:
+          </p>
+
+          {/* Link Row */}
+          <div className="flex flex-row justify-between underline font-bold text-xl md:text-xl leading-relaxed">
+            <div className="col-1/3">
+              <a href="#" className="hover:text-brand-lightBlue hover:scale-105 transition-all">
+                Read Sustainability Simplified
+              </a>
+            </div>
+            <div className="col-1/3">
+              <a href="#" className="hover:text-brand-lightBlue hover:scale-105 transition-all">
+                Download the Workbook
+              </a>
+            </div>
+            <div className="col-1/3">
+              <a href="#" className="hover:text-brand-lightBlue hover:scale-105 transition-all">
+                Join A Workshop
+                </a>
+            </div>
           </div>
+
         </div>
+      </div>
+
+      {/* Images */}
+      <div className="w-full max-w-5xl flex gap-4 p-8">
+        <div className="w-1/2 p-4">
+          <img 
+            src="/images/BookCover.png" 
+            alt="Callout Left" 
+            className="w-full h-full object-cover border-2 border-gray-300 rounded-lg" 
+          />
+        </div>
+        <div className="w-1/2 p-4">
+          <img 
+            src="/images/BookCover_back.png" 
+            alt="Callout Right" 
+            className="w-full h-full object-cover border-2 border-gray-300 rounded-lg" 
+          />
         </div>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@
 
 export default function Hero() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6">
+    <div className="min-h-[98vh] flex flex-col items-center justify-center px-6 border-b border-brand-black">
       <div className="max-w-5xl pl-48">
         <h1 className="text-brand-black font-franklin font-bold text-3xl md:text-5xl uppercase mb-3">
           IMAGINE A WORLD WHERE SUSTAINABILITY IS THE NORM
